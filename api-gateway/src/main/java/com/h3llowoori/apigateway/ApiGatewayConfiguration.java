d0390e0249963615515b9517926bfb254f48d520
h3llowoori <h3llowoori@gmail.com>
2026-02-08 22:52:31 +0900

    feat: Spring Cloud Gateway 프로젝트 구축
    * Spring Cloud Gateway를 사용하여 특정 경로로 들어오는 요청을 다른 서비스(localhost:9001, 9002)로 라우팅하고 경로를 재작성하는 설정 추가
    * 카카오 API는 Rest API 키를 발급받은 후 인증헤더에 이를 추가하여 내가 설정한 Path Rewrite가 잘 동작하고, 헤더 정보가 잘 전달되는지 확인

---
+++ api-gateway/src/main/java/com/h3llowoori/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,34 @@
+package com.h3llowoori.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("book-api", r -> r
+                        .order(1)
+                        .path("/books/**")
+                        .filters(f -> f.rewritePath("/books/(?<segment>.*)", "/v1/books/${segment}"))
+                        .uri("http://localhost:9001"))
+                .route("payment-api", r -> r
+                        .order(1)
+                        .path("/payments/**")
+                        .filters(f -> f.rewritePath("/payments/(?<segment>.*)", "/v1/payments/${segment}"))
+                        .uri("http://localhost:9002"))
+                .route("kakao-book-api", r -> r
+                        .path("/kakao-books/**")
+                        .filters(f -> f
+                                // 1. 경로 재작성: /books/spring -> /v3/search/book?query=spring
+                                .rewritePath("/books/(?<segment>.*)", "/v3/search/book?query=${segment}")
+                                // 2. 카카오 인증 헤더 추가 (반드시 KakaoAK 뒤에 한 칸 띄우고 키 입력)
+                                .addRequestHeader("Authorization", "KakaoAK YOUR_REST_API_KEY"))
+                        .uri("https://dapi.kakao.com"))
+                .build();
+    }
+}
