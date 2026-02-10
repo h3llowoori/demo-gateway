4517cbcc5d12e92fede40cdb36c3ff94a4b0a80f
h3llowoori <h3llowoori@gmail.com>
2026-02-10 23:43:44 +0900

    feat: API Gateway JWT 인증/인가 구현 및 Spring Security 설정
    요약:
     * JWT 기반 인증/인가 시스템 구현 (JwtUtil, AuthenticationManager, SecurityContextRepository)
     * Spring Security WebFlux 설정 (ReactiveSecurityConfig)
     * 로그인 엔드포인트 구현 (AuthenticationController)
     * 패키지 구조 리팩토링 (global/config, global/security)
     * 의존성 추가 (spring-security, jjwt, lombok)
     * 아키텍처 다이어그램 추가 (chapter_3.mermaid)

--- api-gateway/src/main/java/com/h3llowoori/apigateway/global/config/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/h3llowoori/apigateway/global/config/ApiGatewayConfiguration.java
@@ -1,4 +1,4 @@
-package com.h3llowoori.apigateway;
+package com.h3llowoori.apigateway.global.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
